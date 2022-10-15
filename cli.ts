e87a3f32f9cd6eb6c174c2f6ef3b0b2e1483783d
Nicholas Berlette <nick@berlette.com>
2022-10-14 21:40:44 -0700

    feat: add cli.ts file
    Added a simple cli.ts file as a wrapper for the main entry, eggs.ts,
    to help increase compatibility with various other tools. For example,
    the 'land' CLI tool (which allows you to run command-line projects
    without needing to install them first) requires that a project has a
    file named 'cli.ts' to be able to run it. The overall naming convention
    is for module entry points to be named 'mod.ts', command-line entries to
    be named 'cli.ts', and serverless / deno deploy projects' entry points
    to be named 'main.ts[x]'. Using 'eggs.ts' is... odd, but instead of
    renaming the file I decided to wrap it to avoid breaking things. Thx

---
+++ cli.ts
@@ -0,0 +1,8 @@
+#!/usr/bin/env -S deno run -A --unstable
+
+/// <reference no-default-lib="true" />
+/// <reference lib="deno.ns" />
+/// <reference lib="deno.window" />
+/// <reference lib="deno.unstable" />
+
+import "./eggs.ts";
