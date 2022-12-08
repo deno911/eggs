82b900ca27a0463c4fdc8f48f34159832c723a76
Nicholas Berlette <nick@berlette.com>
2022-12-08 06:56:48 -0800

    chore: update dependencies

--- deps.ts
+++ deps.ts
@@ -10,17 +10,17 @@ export {
   type LogMode,
   type LogRecord,
   setup,
-} from "https://deno.land/std@0.166.0/log/mod.ts";
-export * as log from "https://deno.land/std@0.166.0/log/mod.ts";
-export { BaseHandler } from "https://deno.land/std@0.166.0/log/handlers.ts";
-export * from "https://deno.land/std@0.166.0/fmt/colors.ts";
-export * from "https://deno.land/std@0.166.0/testing/asserts.ts";
+} from "https://deno.land/std@0.167.0/log/mod.ts";
 
-export * from "https://deno.land/x/drake@v1.6.0/mod.ts";
+export * as log from "https://deno.land/std@0.167.0/log/mod.ts";
 
-export { prettyBytes } from "https://deno.land/std@0.166.0/fmt/bytes.ts";
+export { BaseHandler } from "https://deno.land/std@0.167.0/log/handlers.ts";
 
-export { default as $ } from "https://deno.land/x/dax@0.15.0/mod.ts";
+export * from "https://deno.land/std@0.167.0/fmt/colors.ts";
+
+export * from "https://deno.land/std@0.167.0/testing/asserts.ts";
+
+export { prettyBytes } from "https://deno.land/std@0.167.0/fmt/bytes.ts";
 
 export {
   basename,
@@ -33,7 +33,7 @@ export {
   normalize as normalizePath,
   relative,
   resolve,
-} from "https://deno.land/std@0.166.0/path/mod.ts";
+} from "https://deno.land/std@0.167.0/path/mod.ts";
 
 export {
   exists,
@@ -42,51 +42,44 @@ export {
   expandGlobSync,
   walk,
   walkSync,
-} from "https://deno.land/std@0.166.0/fs/mod.ts";
-
+} from "https://deno.land/std@0.167.0/fs/mod.ts";
 export {
   parse as parseYaml,
   stringify as stringifyYaml,
-} from "https://deno.land/std@0.166.0/encoding/yaml.ts";
+} from "https://deno.land/std@0.167.0/encoding/yaml.ts";
+
+export * as YAML from "https://deno.land/std@0.167.0/encoding/yaml.ts";
+
+export * as JSONC from "https://deno.land/std@0.167.0/encoding/jsonc.ts";
+
+export * as TOML from "https://deno.land/std@0.167.0/encoding/toml.ts";
 
 export * from "https://deno.land/x/typedefs@0.0.1/egg-json.d.ts";
 
-export * as YAML from "https://deno.land/std@0.166.0/encoding/yaml.ts";
+export * from "https://deno.land/x/drake@v1.6.0/mod.ts";
 
-export * as JSONC from "https://deno.land/std@0.166.0/encoding/jsonc.ts";
-
-export * as TOML from "https://deno.land/std@0.166.0/encoding/toml.ts";
-
-export * from "https://deno.land/x/cliffy/command/mod.ts";
+export { default as $ } from "https://deno.land/x/dax@0.17.0/mod.ts";
 
 export {
   string as stringType,
-} from "https://deno.land/x/cliffy@v0.25.4/flags/types/string.ts";
+} from "https://deno.land/x/cliffy@v0.25.5/flags/types/string.ts";
 
-export * from "https://deno.land/x/cliffy@v0.25.4/prompt/mod.ts";
+export * from "https://deno.land/x/cliffy@v0.25.5/command/mod.ts";
 
-export * from "https://deno.land/x/cliffy@v0.25.4/flags/types.ts";
+export * from "https://deno.land/x/cliffy@v0.25.5/prompt/mod.ts";
+
+export * from "https://deno.land/x/cliffy@v0.25.5/flags/types.ts";
 
 export * as semver from "https://deno.land/x/semver@v1.4.1/mod.ts";
 
 export * as base64 from "https://denopkg.com/chiefbiiko/base64@v0.2.1/mod.ts";
 
-// Resolved arweave location for hatcher@0.10.2
 export * from "https://x.nest.land/hatcher@0.10.2/mod.ts";
-// export * from "https://pptkwtismwjswyniwwx3z7bu67wzsjknnmqs3zo5vmd4vr7fqswq.arweave.net/e-arTRJlkythqLWvvPw09-2ZJU1rIS3l3asHysflhK0/mod.ts";
 
-// Resolved arweave location for hatcher@0.10.2
 export * from "https://x.nest.land/hatcher@0.10.2/lib/utilities/utils.ts";
-// export * from "https://pptkwtismwjswyniwwx3z7bu67wzsjknnmqs3zo5vmd4vr7fqswq.arweave.net/e-arTRJlkythqLWvvPw09-2ZJU1rIS3l3asHysflhK0/lib/utilities/utils.ts";
 
-// Resolved arweave location for hatcher@0.10.2
 export { install as installHatcher } from "https://x.nest.land/hatcher@0.10.2/lib/cli.ts";
-// export {
-// install as installHatcher,
-// } from "https://pptkwtismwjswyniwwx3z7bu67wzsjknnmqs3zo5vmd4vr7fqswq.arweave.net/e-arTRJlkythqLWvvPw09-2ZJU1rIS3l3asHysflhK0/lib/cli.ts";
 
-// Resolved arweave location for analyzer@0.0.6
 export * from "https://x.nest.land/analyzer@0.0.6/deno/tree.ts";
-// export * from "https://3cfd3lulgczqtos7nd7bcmymng2nybpbdzsdljvpo64uo3vyx4kq.arweave.net/2Io9roswswm6X2j-ETMMabTcBeEeZDWmr3e5R264vxU/deno/tree.ts";
 
 export * from "https://deno.land/x/wait@0.1.12/mod.ts";
